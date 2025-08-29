b81a3633dbd2fc2adcb08f9066090245025031af
MATHEUS MIGUEL BARBOSA <matheus.miguel@cs.up.edu.br>
2025-08-29 11:32:50 -0300

    API _ WEB

--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,6 +1,16 @@
+Console.Clear();
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//Funcionalidades
+//Requisições
+// - Endereço
+// - Método HTTP
+app.MapGet("/", () => "Estou com sonoooooooooo");
+
+app.MapGet("/funcnionalidade", () => "Segundo sono");
+
+app.MapPost("/funcnionalidade", () => "Terceiro sono");
 
 app.Run();
